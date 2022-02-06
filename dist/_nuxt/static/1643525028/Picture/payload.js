12552496f5f19f74f4d600ddc5a5d6fb22460faf
sh-kikuchi <shu94hist726@gmail.com>
2022-02-06 13:57:45 +0900

    feature_12_220207_update

--- dist/_nuxt/static/1643525028/Picture/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/picture", {data:[{}],fetch:{},mutations:[]});
--- dist/_nuxt/static/1643525028/blog/nuxt_vue/{navigation}/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643525028",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fblog\u002Fnuxt_vue\u002F%7Bnavigation%7D",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4101f95e"}}}}(null));
