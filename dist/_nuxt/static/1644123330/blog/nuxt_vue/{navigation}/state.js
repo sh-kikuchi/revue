2bb7631f77e7ecb0a358443f8e4a8d7a030eb7e0
sh-kikuchi <shu94hist726@gmail.com>
2022-02-08 00:29:32 +0900

    feature_12_220207_update

--- dist/_nuxt/static/1644123330/blog/nuxt_vue/{navigation}/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644123330",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fblog\u002Fnuxt_vue\u002F%7Bnavigation%7D",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cf32b174"}}}}(null));
