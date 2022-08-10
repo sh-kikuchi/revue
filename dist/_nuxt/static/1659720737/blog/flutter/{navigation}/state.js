85d4b59d10d3b2c5f073081279e81b01588e8fe2
sh-kikuchi <shu94hist726@gmail.com>
2022-08-10 20:53:25 +0900

    setting_favicon

--- dist/_nuxt/static/1659720737/blog/flutter/{navigation}/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659720737",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fblog\u002Fflutter\u002F%7Bnavigation%7D",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8cb35a5b"}}}}(null));
